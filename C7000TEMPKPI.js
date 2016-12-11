91ed90a2b46d8cd7a76b65563135b2c0ed1dcd3e
juanmauricio <juan.mauricio.lopez@gmail.com>
2016-12-10 23:42:34 -0500

    con los ejes y limites

--- C7000TEMPKPI.js
+++ C7000TEMPKPI.js
@@ -5,54 +5,62 @@ var customStoreC7000Temp = new DevExpress.data.CustomStore({
         var d = $.Deferred();
         $.getJSON('http://localhost:3000/c7000temp').done(function (data) {
             d.resolve(data.c7000temp, { totalCount: data.c7000temp.length });
-            C7000TempMeasure = data.c7000temp[data.c7000temp.length-1].value;
+            C7000TempMeasure = data.c7000temp[data.c7000temp.length - 1].value;
         });
         return d.promise();
     }
 });
 
-setInterval(function(){
-  customStoreC7000Temp.load();
+setInterval(function () {
+    customStoreC7000Temp.load();
 
-  $("#C7000TempChart").dxChart({
-      dataSource: customStoreC7000Temp,
-      commonSeriesSettings: {
-          argumentField: 'measuredatetime'
-      },
-      series: [
-          { name: 'CISCO Temp', valueField: 'value', showInLegend: false }
-      ]
-  });
+    $("#C7000TempChart").dxChart({
+        dataSource: customStoreC7000Temp,
+        commonSeriesSettings: {
+            argumentField: 'measuredatetime'
+        },
+        series: [
+            { name: 'CISCO Temp', valueField: 'value', showInLegend: false }
+        ],
+        argumentAxis: {
+            label: {
+                overlappingBehavior: {
+                    mode: "rotate",
+                    rotationAngle: 270
+                }
+            }
+        },
+    });
 
-$("#C7000TempChartcircularGauge").dxCircularGauge({
-  title: {
-      text: 'C7000 TEMP',
-      font: {
-          size: 30,
-          weight: 400
-      },
-      subtitle: {
-          text: 'Mb',
-          font: {
-              size: 15,
-              weight: 100
-          }
-      }
-  },
-    scale: {
-        startValue: 0,
-        endValue: 100
-    },
-    value: Number(C7000TempMeasure),
-    subvalues: [70,100],
-    valueIndicator: {type: 'rangeBar'},
-    //subvalueIndicator: {type: 'textCloud'},
-    rangeContainer: {
-        ranges: [
-            { startValue: 0, endValue: 40,  color: 'green' },
-            { startValue: 60,   endValue: 100, color: 'red' }
-        ]
-    }
-});
+    $("#C7000TempChartcircularGauge").dxCircularGauge({
+        title: {
+            text: 'C7000 TEMP',
+            font: {
+                size: 30,
+                weight: 400
+            },
+            subtitle: {
+                text: 'Mb',
+                font: {
+                    size: 15,
+                    weight: 100
+                }
+            }
+        },
+        scale: {
+            startValue: 0,
+            endValue: 100
+        },
+        value: Number(C7000TempMeasure),
+        subvalues: [70, 100],
+        valueIndicator: { type: 'rangeBar' },
+        //subvalueIndicator: {type: 'textCloud'},
+        rangeContainer: {
+            ranges: [
+                { startValue: 0, endValue: 40, color: 'green' },
+                { startValue: 60, endValue: 100, color: 'red' }
+            ]
+        }
+    });
 
 }, 5000);
--- CISCOTEMPKPI.js
+++ CISCOTEMPKPI.js
@@ -5,54 +5,78 @@ var customStoreCISCOTemp = new DevExpress.data.CustomStore({
         var d = $.Deferred();
         $.getJSON('http://localhost:3000/ciscotemp/').done(function (data) {
             d.resolve(data.ciscotemp, { totalCount: data.ciscotemp.length });
-            CISCOTempMeasure = data.ciscotemp[data.ciscotemp.length-1].value;
+            CISCOTempMeasure = data.ciscotemp[data.ciscotemp.length - 1].value;
         });
         return d.promise();
     }
 });
 
-setInterval(function(){
-  customStoreCISCOTemp.load();
+setInterval(function () {
+    customStoreCISCOTemp.load();
 
-  $("#CISCOTempChart").dxChart({
-      dataSource: customStoreCISCOTemp,
-      commonSeriesSettings: {
-          argumentField: 'measuredatetime'
-      },
-      series: [
-          { name: 'SQL HDD', valueField: 'value', showInLegend: false }
-      ]
-  });
-
-$("#CISCOTempChartcircularGauge").dxCircularGauge({
-  title: {
-      text: 'CISCO TEMP',
-      font: {
-          size: 30,
-          weight: 400
-      },
-      subtitle: {
-          text: 'Mb',
-          font: {
-              size: 15,
-              weight: 100
-          }
-      }
-  },
-    scale: {
-        startValue: 0,
-        endValue: 100
-    },
-    value: Number(CISCOTempMeasure),
-    subvalues: [70,100],
-    valueIndicator: {type: 'rangeBar'},
-    //subvalueIndicator: {type: 'textCloud'},
-    rangeContainer: {
-        ranges: [
-            { startValue: 0, endValue: 40,  color: 'green' },
-            { startValue: 60,   endValue: 100, color: 'red' }
+    $("#CISCOTempChart").dxChart({
+        dataSource: customStoreCISCOTemp,
+        commonSeriesSettings: {
+            argumentField: 'measuredatetime'
+        },
+        series: [
+            { name: 'SQL HDD', valueField: 'value', showInLegend: false }
+        ],
+        argumentAxis: {
+            label: {
+                overlappingBehavior: {
+                    mode: "rotate",
+                    rotationAngle: 270
+                }
+            }
+        },
+        valueAxis: [
+            {
+                constantLines: [
+                    {
+                        color: "#FF0000",
+                        value: 20,
+                        label: {
+                            text: 20,
+                            visible: true,
+                            position: "inside",
+                            horizontalAlignment: "right"
+                        }
+                    }
+                ]
+            }
         ]
-    }
-});
+    });
+
+    $("#CISCOTempChartcircularGauge").dxCircularGauge({
+        title: {
+            text: 'CISCO TEMP',
+            font: {
+                size: 30,
+                weight: 400
+            },
+            subtitle: {
+                text: 'Mb',
+                font: {
+                    size: 15,
+                    weight: 100
+                }
+            }
+        },
+        scale: {
+            startValue: 0,
+            endValue: 100
+        },
+        value: Number(CISCOTempMeasure),
+        subvalues: [70, 100],
+        valueIndicator: { type: 'rangeBar' },
+        //subvalueIndicator: {type: 'textCloud'},
+        rangeContainer: {
+            ranges: [
+                { startValue: 0, endValue: 40, color: 'green' },
+                { startValue: 60, endValue: 100, color: 'red' }
+            ]
+        }
+    });
 
 }, 5000);
